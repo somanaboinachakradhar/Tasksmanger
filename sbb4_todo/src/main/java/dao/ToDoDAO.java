26ab36f020795bdf350bb1812997d0950517ea9d
somanaboinachakradhar <chakrisomanaboina432@gmail.com>
2025-03-24 14:38:48 +0530

    Added new folder

---
+++ sbb4_todo/src/main/java/dao/ToDoDAO.java
@@ -0,0 +1,21 @@
+// ToDoDAO.java
+package dao;
+
+import java.util.List;
+
+import beans.Register;
+import beans.Task;
+
+public interface ToDoDAO {
+	public int register(Register register);
+
+	public int login(String email, String pass);
+
+	public List<Task> findTasksByRegId(int regId);
+
+	public int addTask(Task task, int regId);
+
+	public boolean markTaskCompleted(int taskId, int regId);
+
+	public String getFLNameByRegID(int regId);
+}
